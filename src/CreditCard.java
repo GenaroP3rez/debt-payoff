f6e8d4adf8ae0edec68e6b9ad8f60fec65e3d649
lilPunny <166659824+GenaroP3rez@users.noreply.github.com>
2026-04-20 11:42:03 -0700

    added scanners, getters and setters

--- src/CreditCard.java
+++ src/CreditCard.java
@@ -1,4 +1,32 @@
 public class CreditCard {
-    
-    
+
+    private String name; 
+    private double apr;
+    private double balance; 
+
+    @Override 
+    public String toString(){
+            return "CreditCard [name=" + name + ", apr=" + apr + ", balance=" + balance + "]";
+    }
+
+    public CreditCard(String something, double apr, double balance){
+        name = something; 
+        this.apr = apr;
+        this.balance = balance;
+    }
+
+
+    public String getName(){
+        return name;
+    }
+
+    public double getApr(){
+        return apr;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+
 }
--- src/PayoffApp.java
+++ src/PayoffApp.java
@@ -1,25 +1,31 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class PayoffApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        CreditCard amex = new CreditCard("American Express", 18.4, 402);
+
+
+
         //added//type inference "'an arraylist of double"
         List<Double> aprs = new ArrayList<>();
+        List<CreditCard> card = new ArrayList<>();
 
         while(scan.hasNextLine()) {
             String name = scan.nextLine();
 
             double apr = scan.nextDouble();
 
-            //added code
+
             aprs.add(apr);
 
-
-
             double balance = scan.nextDouble();
+            CreditCard myCard = new CreditCard(name, apr, balance);
+            card.add(myCard);
 
             // Consume \n after balance input 
             if(scan.hasNextLine()) scan.nextLine();
@@ -30,8 +36,11 @@ public class PayoffApp {
         }
 
         Collections.sort(aprs, Collections.reverseOrder()); 
+        // "you didnt tell me how to compare it" compare interface is next class 4/22/26
+       // Collections.sort(card); 
 
         //added code
         System.out.println(aprs);
+        System.out.println(card);
     }
 }
